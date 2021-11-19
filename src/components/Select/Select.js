3ea12ee465ed4e56fd9a8619bc54b5b71469c2c5
Anton Yurchenko <webbymasta@gmail.com>
2021-11-19 19:01:59 +0200

    Select element

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+    const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+    return (
+        <Wrapper>
+            {displayedValue}
+            <Chevrone id="chevron-down" size={24} strokeWidth={2} />
+            <HiddenSelect value={value} onChange={onChange}>
+                {children}
+            </HiddenSelect>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    width: fit-content;
+    position: relative;
+    padding: 12px 52px 12px 16px;
+    font-size: 16px;
+    line-height: 19px;
+    color: ${COLORS.gray700};
+    background-color: ${COLORS.transparentGray15};
+    border: 0;
+    border-radius: 8px;
+
+    &:focus {
+        outline: 2px solid ${COLORS.primary};
+        outline-offset: 0;
+    }
+
+    &:hover {
+        color: ${COLORS.black};
+    }
+`;
+
+const Chevrone = styled(Icon)`
+    position: absolute;
+    top: 10px;
+    right: 8px;
+    color: inherit;
+`;
+
+const HiddenSelect = styled.select`
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+`;
 
 export default Select;
